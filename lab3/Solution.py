4115f03778d8e5f213b18f4760cd539e481d5be1
Miłosz Sobol <miloszsobolsci@gmail.com>
2025-03-26 17:45:33 +0100

    completed lab3 (codewise)

---
+++ lab3/Solution.py
@@ -0,0 +1,91 @@
+import hashlib
+import random
+import time
+from Utils import Utils
+
+
+class Solution:
+
+    def __init__(self) -> None:
+        self.keys = ["MD5", "SHA1", "SHA2", "SHA3"]
+        self.functionNames = [self.generateMD5, self.generateSHA1, self.generateSHA2, self.generateSHA3]
+        self.utils = Utils.Utils()
+
+    def generateAll(self,text: str):
+        res = dict()
+        keys = self.keys
+        functionNames = self.functionNames
+        times = dict()
+
+        for idx, functionName in enumerate(functionNames):
+            start = time.time_ns()
+            res[keys[idx]]= functionName(text.encode())
+            end = time.time_ns()
+            times[keys[idx]] = end - start
+
+        return res, times
+
+
+
+    def generateMD5(self,text) -> str:
+        return hashlib.md5(text).hexdigest()
+
+    
+    def generateSHA1(self, text) -> str:
+        return hashlib.sha1(text).hexdigest()
+
+    def generateSHA2(self, text) -> str:
+        return hashlib.sha256(text).hexdigest()
+
+    def generateSHA3(self, text) -> str:
+        return hashlib.sha384(text).hexdigest()
+
+    def testCollision(self, encodedText, numberOfTests,functionName):
+        textLen = len(encodedText)
+        numberOfCollisions = 0
+        testingHash = functionName(encodedText)[:3]
+
+        for i in range(1, numberOfTests):
+            randomString = self.utils.generateRandomStringOfLength(textLen).encode()
+            if testingHash == functionName(randomString)[:3]:
+                numberOfCollisions += 1
+        return numberOfCollisions
+
+    def countBits(self,byteSeq):
+        binary_representation = ''.join(bin(byte)[2:].zfill(8) for byte in byteSeq)
+        numOnes = binary_representation.count('1')
+        numZeros = binary_representation.count('0')
+        return numZeros, numOnes
+
+    def bitChangeProbability(self,originalHash, newHash):
+        xor_result = bytes(a ^ b for a, b in zip(originalHash, newHash))
+
+        _, changedBits = self.countBits(xor_result)
+
+        totalBits = len(originalHash) * 8
+        changeProbability = changedBits / totalBits
+
+        return changeProbability
+
+    def testSAC(self, encodedText, functionName, iterations):
+        originalText = encodedText
+        originalHash = functionName(originalText)
+        textLen = len(encodedText)
+
+        probabilities = dict()
+
+        for i in range(iterations):
+
+            randomPosition = random.randint(0, textLen-1)
+            encodedTextArray = bytearray(encodedText)
+            encodedTextArray[randomPosition] = encodedTextArray[randomPosition] ^ 1
+
+            newText = bytes(encodedTextArray)
+            newHash = functionName(newText)
+
+            changeProbability = self.bitChangeProbability(bytes(originalHash.encode()), bytes(newHash.encode()))
+
+            probabilities[i] = changeProbability
+
+        return probabilities
+
---
+++ lab3/main.py
@@ -0,0 +1,47 @@
+import Solution
+
+def generateRandomStrings():
+    import random
+    import string
+    testingStrings = []
+    for i in range(4,100,10):
+        testingStrings.append(''.join(random.choices(string.ascii_uppercase + string.digits, k = i)))
+    return testingStrings
+
+def main():
+    #zad2
+    testingStrings = generateRandomStrings()
+    solution = Solution.Solution()
+    timesDict = dict()
+    results = dict()
+
+    for string in testingStrings:
+        res,times = solution.generateAll(string)
+        results[len(string)] = {key: len(res[key]) for key in res}
+        timesDict[len(string)] = times
+    #Niezaleznie od dlugosci stringa, MD5: 32. SHA1: 40, SHA2: 64, SHA3: 96
+    #Dodatkowo funkcje sa na tyle szybkie ze nie mozliwe jest zmierzenie ich czasu wykonania (za pomoca time_ns)
+
+    #zad3
+    generated = solution.generateMD5("test".encode())
+    #print(generated)
+    #wartosc wygenerowana: 098f6bcd4621d373cade4e832627b4f6 jest powszechnie znana jako hash dla slowa "test"
+    #Skróty wygenerowane dla krótkich i popularnych haseł nie są bezpieczne ponieważ znajdują się w jawnych słownikach umożliwiających
+    #zgadnięcie takiego hasła
+
+    #zad4
+    #Funkcje MD5 nie może zostać uznana za bezpieczną, juz w 2004, wraz z zespołem, Wang wykazał, że jest ona podatna na kolizje
+    #Od tego czasu powstało wiele metod ataków na MD5, które umożliwiają złamanie hasła w krótkim czasie
+    #Do tego MD5 jest szybka w porównaniu do SHA-2 i SHA-3, co sprawia, że jest ona bardziej podatna na ataki brute-force
+
+    #zad5
+    testedString = "verylongstring".encode()
+    #print(solution.testCollision(testedString, 100000, solution.generateMD5))
+
+    #zad6
+    testedString = "verylongstring".encode()
+    print(solution.testSAC(testedString, solution.generateMD5, 1))
+
+
+if __name__ == "__main__":
+    main()
