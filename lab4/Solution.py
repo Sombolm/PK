426b02f7fde40aa731e6127f4f3bce0df176fcc4
Miłosz Sobol <miloszsobolsci@gmail.com>
2025-04-04 03:44:39 +0200

    done

--- lab4/Solution.py
+++ lab4/Solution.py
@@ -9,7 +9,7 @@ class Solution:
         return AES.new(key, AES.MODE_CBC, iv=iv)
 
     def createCipherCTR(self, key, nonce):
-        return AES.new(key, AES.MODE_CTR, nonce=nonce)
+        return AES.new(key, AES.MODE_CTR, nonce = b'1',initial_value=0)
 
     def encrypt(self, cipher, text):
         return cipher.encrypt(text)
--- lab4/main.py
+++ lab4/main.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import Solution
 from Utils import Utils
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 
 solution = Solution.Solution()
 utils = Utils.Utils()
@@ -54,28 +54,42 @@ def plotTimes(avgTimes):
     plt.show()
 
 def flipFirstBitOfMessage(message):
-    flipped_message = bytearray(message, 'utf-8')
-    flipped_message[len(flipped_message) // 2] ^= 1
-    return flipped_message.decode('utf-8', errors='ignore')
+    flipped_message = bytearray(message)
+    flipped_message[0] ^= 0b00000001
+    return bytes(flipped_message)
 
 def flipFirstBitOfCipher(cipher):
     flipped_cipher = bytearray(cipher)
-    flipped_cipher[0] ^= 1
-    return flipped_cipher
-def compareBitWiseError(original, damaged):
-    numberOfErrors = 0
+    original = flipped_cipher.copy()
+    flipped_cipher[0] ^= 0b00000001
+    for i in range(len(original)):
+        if original[i] != flipped_cipher[i]:
+            print("Flipped byte at index", i, "from", original[i], "to", flipped_cipher[i])
+
+    return bytes(flipped_cipher)
+
+
+def compareBlockWiseError(original, damaged, blockSize=16):
+    damaged_blocks = 0
 
     original = bytearray(original)
     damaged = bytearray(damaged)
 
-    for i in range(min(len(original), len(damaged))):
-        xor = original[i] ^ damaged[i]
-        numberOfErrors += bin(xor).count("1")
+    numBlocks = min(len(original), len(damaged)) // blockSize
 
-    return numberOfErrors
+    for i in range(numBlocks):
+        blockStart = i * blockSize
+        blockEnd = blockStart + blockSize
+
+        originalBlock = original[blockStart:blockEnd]
+        damagedBlock = damaged[blockStart:blockEnd]
+
+        if originalBlock != damagedBlock:
+            damaged_blocks += 1
+
+    return damaged_blocks
 
 def createAllCiphers(key, iv, nonce):
-    key = b'0123456789abcdef'
     cipherECB = solution.createCipherECB(key)
     cipherCBC = solution.createCipherCBC(key, iv)
     cipherCTR = solution.createCipherCTR(key, nonce)
@@ -109,11 +123,11 @@ def plotErrors(sumErrors):
     plt.ylim(0, 100)
     plt.yticks(range(0, 101, 10))
 
-
+    '''
     for bar in bars:
         yval = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2, yval + 2, f'{yval:.2f}%', ha='center', va='bottom')
-
+    '''
 
     plt.subplot(1, 2, 2)
     bars = plt.bar(scenario2.keys(), scenario2.values(), color=['blue', 'green', 'red'])
@@ -123,10 +137,11 @@ def plotErrors(sumErrors):
     plt.ylim(0, 100)
     plt.yticks(range(0, 101, 10))
 
+    '''
     for bar in bars:
         yval = bar.get_height()
         plt.text(bar.get_x() + bar.get_width() / 2, yval + 2, f'{yval:.2f}%', ha='center', va='bottom')
-
+    '''
     plt.tight_layout()
     plt.show()
 
@@ -156,31 +171,31 @@ def main():
     for file in files:
         print(f"File {file} generated")
 
-    key = b'0123456789abcdef'
+    keyForCypher = b'0123456789abcdef'
     nonce = random.getrandbits(64).to_bytes(8, 'big')
     iv = random.getrandbits(128).to_bytes(16, 'big')
     iterations = 40
 
-    cipherECB, cipherCBC, cipherCTR = createAllCiphers(key, iv, nonce)
+    cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
 
     ciphers = [cipherECB, cipherCBC, cipherCTR]
     tags = ["ECB", "CBC", "CTR"]
     times = dict()
     
-    for i in range(iterations):
+    for i in range(0):
         print("Iteration", i)
         for idx, cipher in enumerate(ciphers):
             for file in filenames:
 
-                cipher = solution.createCipherCBC(key, iv) if tags[idx] == "CBC" else cipher
-                cipher = solution.createCipherCTR(key, nonce) if tags[idx] == "CTR" else cipher
+                cipher = solution.createCipherCBC(keyForCypher, iv) if tags[idx] == "CBC" else cipher
+                cipher = solution.createCipherCTR(keyForCypher, nonce) if tags[idx] == "CTR" else cipher
 
                 elapsedTime, encrypted = mesaureEncryptionTime(cipher, file)
                 times[(tags[idx] , file, i,'encrypted')] = elapsedTime, encrypted
                 #print(f"Encryption time for {file} using {tags[idx]}: {elapsedTime} ms")
 
-                cipher = solution.createCipherCBC(key, iv) if tags[idx] == "CBC" else cipher
-                cipher = solution.createCipherCTR(key, nonce) if tags[idx] == "CTR" else cipher
+                cipher = solution.createCipherCBC(keyForCypher, iv) if tags[idx] == "CBC" else cipher
+                cipher = solution.createCipherCTR(keyForCypher, nonce) if tags[idx] == "CTR" else cipher
 
                 elapsedTime, decrypted = mesaureDecryptionTime(cipher, encrypted)
                 times[(tags[idx] , file,i ,'decrypted')] = elapsedTime, decrypted
@@ -197,38 +212,41 @@ def main():
 
     # zad 2
     sumErrors = defaultdict(float)
-    for i in range(iterations):
+    for i in range(1):
+        iv = random.getrandbits(128).to_bytes(16, 'big')
         randomMessage = utils.generateRandomStringOfLength(50)
 
-        cipherECB, cipherCBC, cipherCTR = createAllCiphers(key, iv, nonce)
+        cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
         paddedMessage = padMessage(randomMessage)
 
-        encryptedECB = solution.encrypt(cipherECB, paddedMessage)
-        encryptedCBC = solution.encrypt(cipherCBC, paddedMessage)
-        encryptedCTR = solution.encrypt(cipherCTR, paddedMessage)
+        encryptedECB = solution.encrypt(cipherECB, bytearray(paddedMessage))
+        encryptedCBC = solution.encrypt(cipherCBC, bytearray(paddedMessage))
+        encryptedCTR = solution.encrypt(cipherCTR, bytearray(paddedMessage))
 
-        damagedMessage = flipFirstBitOfMessage(randomMessage)
-        paddedDamagedMessage = padMessage(damagedMessage)
+        damagedMessage = flipFirstBitOfMessage(bytearray(paddedMessage))
+        paddedDamagedMessage = damagedMessage
 
-        damagedMessageEncryptedECB = solution.encrypt(cipherECB, paddedDamagedMessage)
-        damagedMessageEncryptedCBC = solution.encrypt(cipherCBC, paddedDamagedMessage)
-        damagedMessageEncryptedCTR = solution.encrypt(cipherCTR, paddedDamagedMessage)
+        cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
+
+        damagedMessageEncryptedECB = solution.encrypt(cipherECB, bytearray(paddedDamagedMessage))
+        damagedMessageEncryptedCBC = solution.encrypt(cipherCBC, bytearray(paddedDamagedMessage))
+        damagedMessageEncryptedCTR = solution.encrypt(cipherCTR, bytearray(paddedDamagedMessage))
 
         for tag, encrypted, damagedEncrypted in zip(["ECB", "CBC", "CTR"], [encryptedECB, encryptedCBC, encryptedCTR],
                                                     [damagedMessageEncryptedECB, damagedMessageEncryptedCBC, damagedMessageEncryptedCTR]):
-            numberOfErrors = compareBitWiseError(encrypted, damagedEncrypted)
+            numberOfErrors = compareBlockWiseError(encrypted, damagedEncrypted)
             sumErrors[tag, 's1'] += numberOfErrors
 
         #scenario 2
         randomMessage = utils.generateRandomStringOfLength(50)
         paddedMessage = padMessage(randomMessage)
 
-        cipherECB, cipherCBC, cipherCTR = createAllCiphers(key, iv, nonce)
+        cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
 
         #tworzenie szyfrogramu
-        encryptedECB = solution.encrypt(cipherECB, paddedMessage)
-        encryptedCBC = solution.encrypt(cipherCBC, paddedMessage)
-        encryptedCTR = solution.encrypt(cipherCTR, paddedMessage)
+        encryptedECB = solution.encrypt(cipherECB, bytearray(paddedMessage))
+        encryptedCBC = solution.encrypt(cipherCBC, bytearray(paddedMessage))
+        encryptedCTR = solution.encrypt(cipherCTR, bytearray(paddedMessage))
 
         damagedEncryptionECB = flipFirstBitOfCipher(encryptedECB)
         damagedEncryptionCBC = flipFirstBitOfCipher(encryptedCBC)
@@ -236,26 +254,26 @@ def main():
 
 
         #rozszyfrowanie
-        cipherECB, cipherCBC, cipherCTR = createAllCiphers(key, iv, nonce)
+        cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
 
-        decryptedECB = solution.decrypt(cipherECB, encryptedECB)
-        decryptedCBC = solution.decrypt(cipherCBC, encryptedCBC)
-        decryptedCTR = solution.decrypt(cipherCTR, encryptedCTR)
+        decryptedECB = solution.decrypt(cipherECB, bytearray(encryptedECB))
+        decryptedCBC = solution.decrypt(cipherCBC, bytearray(encryptedCBC))
+        decryptedCTR = solution.decrypt(cipherCTR, bytearray(encryptedCTR))
 
-        cipherECB, cipherCBC, cipherCTR = createAllCiphers(key, iv, nonce)
+        cipherECB, cipherCBC, cipherCTR = createAllCiphers(keyForCypher, iv, nonce)
 
-        decryptedDamagedECB = solution.decrypt(cipherECB, damagedEncryptionECB)
-        decryptedDamagedCBC = solution.decrypt(cipherCBC, damagedEncryptionCBC)
-        decryptedDamagedCTR = solution.decrypt(cipherCTR, damagedEncryptionCTR)
+        decryptedDamagedECB = solution.decrypt(cipherECB, bytearray(damagedEncryptionECB))
+        decryptedDamagedCBC = solution.decrypt(cipherCBC, bytearray(damagedEncryptionCBC))
+        decryptedDamagedCTR = solution.decrypt(cipherCTR, bytearray(damagedEncryptionCTR))
         for tag, decrypted, decryptedDamaged in zip(["ECB", "CBC", "CTR"], [decryptedECB, decryptedCBC, decryptedCTR],
                                                     [decryptedDamagedECB, decryptedDamagedCBC, decryptedDamagedCTR]):
-            numberOfErrors = compareBitWiseError(decrypted, decryptedDamaged)
+            numberOfErrors = compareBlockWiseError(decrypted, decryptedDamaged)
             sumErrors[tag, 's2'] += numberOfErrors
 
-    byteLenght = len(decrypted)
+    byteLenght = len(bytes(decrypted))
 
     for key in sumErrors:
-        sumErrors[key] = (sumErrors[key] / iterations) / (byteLenght * 8) * 100
+        sumErrors[key] = (sumErrors[key]) / (byteLenght / 16) * 100
     plotErrors(sumErrors)
 
 if __name__ == "__main__":
