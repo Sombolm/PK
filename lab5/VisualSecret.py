65f1fd208aee179cfb103322fb04c0d01c9a24fa
sombol <miloszsobolsci@gmail.com>
2025-04-23 14:07:21 +0200

    visualsecret

---
+++ lab5/VisualSecret.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+class VisualSecret:
+    def __init__(self):
+        pass
+
+
+
+
+    def encrypt(self, imagePath):
+        def splitPixel(pixel):
+            if pixel == 0:
+
+
+            else:
+
+
+
+        image = Image.open(imagePath)
+        image = image.convert("1")
+
+
+
+        for x in range(image.width):
+            for y in range(image.height):
